e0cb9dbff374ca8f97664f8c6fc6c16d2c777f36
ShiRaw11 <shiikimani11@gmail.com>
2023-04-27 00:43:07 -0700

    task 0 to task 9 : 0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+  *main -prints all numbers of base 10
+  *return -Always 0 (success)
+  */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d",number);
+
+	printf("\n");
+
+	return (0);
+}
+
+
+
+
