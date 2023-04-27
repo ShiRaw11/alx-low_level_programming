96203e6297d6c0604ea9c56ce6ba13456805fcf2
ShiRaw11 <shiikimani11@gmail.com>
2023-04-27 08:12:20 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
-  *main -prints all numbers of base 10
-  *return -Always 0 (success)
+  *main - Prints all numbers of base 10
+  *Return: Always 0 (success)
   */
 int main(void)
 {
