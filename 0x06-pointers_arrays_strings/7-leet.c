60a74b93c07670c8218a3cdf1d6d03c3ae43d940
ShiRaw11 <shiikimani11@gmail.com>
2023-05-05 14:07:19 -0700

    task 7 and 8

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  *leet - encodes a string into 1337.
+  *@s: string
+  *Return: Always 0(success)
+  */
+
+char *leet(char *s)
+{
+
+	int i, j;
+
+	char *x = "aAeEoOtTlL";
+	char *y = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == x[j])
+			{
+				s[i] = y[j];
+			}
+		}
+	}
+
+	return (0);
+}
+
